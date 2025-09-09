d798e5ada99a74cbe6ea6ad22a5d0ff516cbf0f2
Carlos Castillo Martinez <ccmartinez@uoc.edu>
2025-09-09 12:12:04 +0200

    feat: button component

--- src/components/femButton.ts
+++ src/components/femButton.ts
@@ -1,15 +1,28 @@
-import twSheet, { twCssText } from "../styles/tw-shadow";
+import twSheet from "../styles/tw-shadow";
 
 const template = document.createElement("template");
 template.innerHTML = `
   <button
-    class="bg-fem-green-200 p-4 text-preset-3 uppercase hover:bg-fem-grey-800 hover:border-2 hover:border-fem-green-200"
+    type="button"
+    class="
+      p-4 
+      text-preset-3 
+      uppercase 
+      bg-fem-green-200 
+      border-2 
+      border-transparent
+      cursor-pointer
+      transition-colors
+      hover:bg-fem-grey-800 
+      hover:border-fem-green-200
+      hover:text-fem-green-200
+    "
   >
     <slot></slot>
   </button>
 `;
 
-export class femButton extends HTMLElement {
+export class FemButton extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
@@ -18,4 +31,4 @@ export class femButton extends HTMLElement {
   }
 }
 
-customElements.define("fem-button", femButton);
+customElements.define("fem-button", FemButton);
